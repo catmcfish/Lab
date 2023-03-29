85bfbd1d84b2e512b1f4911230373944f1b594ae
Noah Gaw <noahgaw@msu.edu>
2023-03-29 22:27:07 +0000

    Added skinwalker reference

--- example.cpp
+++ example.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 int main(){
-	std::cout << "You are not real";
+	bool ihavenoidea;
+	if(YOUARENOTREAL(ihavenoidea)){
+	std::cout << "You are real i guess";}
+	else{
+	std::cout << "skinwalker headass get away from me";
+	}
+}
+bool YOUARENOTREAL(bool areyoureal){
+if(areyoureal){
+return true;
+}
+else{
+return false;
+}
 }
